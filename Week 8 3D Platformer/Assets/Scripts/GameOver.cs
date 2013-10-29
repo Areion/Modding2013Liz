f61de93a1a1be92cd9c50d18e300cb1afd5fa47a
Elizabeth Hendren <eah415@nyu.edu>
2013-10-29 09:31:30 -0400

    this is stuff

---
+++ Week 8 3D Platformer/Assets/Scripts/GameOver.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter () {
+		guiText.text = "Game Over";
+		audio.Play();
+		
+	}
+}
